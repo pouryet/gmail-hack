cf918eb59f516787e2a80bc250557d445cbb67db
pouryet <106576397+pouryet@users.noreply.github.com>
2023-12-23 14:41:47 +0330

    Extraction.py

---
+++ Extraction.py
@@ -0,0 +1,16 @@
+import win32com.client
+
+outlook = win32com.client.Dispatch("Outlook.Application")
+namespace = outlook.GetNamespace("MAPI")
+
+accounts = namespace.Accounts
+
+with open("email_accounts.txt", "w") as file:
+    for account in accounts:
+        email = account.SmtpAddress
+        password = None
+        try:
+            password = account.Password
+        except Exception as e:
+            print(f"Could not get password for account {email}: {str(e)}")
+        file.write(f"Email: {email}, Password: {password}\n")
